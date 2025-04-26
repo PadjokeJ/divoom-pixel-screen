2079bc8e2d77b3d632fecbad528f2544110ee91e
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-04-26 14:53:32 +0200

    rename files in plugins, plus include way to get spotify token easily

--- plugins/spotify/spotify_reader.py
+++ plugins/spotify/spotify_reader.py
@@ -1,7 +1,7 @@
 import requests
 from os import getenv
 from dotenv import load_dotenv
-load_dotenv("local.env", verbose=True)
+load_dotenv("../../local.env", verbose=True)
 from io import BytesIO
 
 hostname = "https://api.spotify.com/v1/"
@@ -36,6 +36,33 @@ def progress_bar():
 def album_cover(url):
     response = s.get(url)
     return BytesIO(response.content)
+
+def get_auth_from_user():
+    import requests
+    import webbrowser
+
+    redirect_uri = "http://localhost"
+
+    # Request authorization from the user
+    auth_url = f"https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&scope=user-read-playback-state user-modify-playback-state"
+    response = requests.get(auth_url)
+
+    # Open in browser, to log in to spotify
+    webbrowser.open(response.url)
+
+def get_token():
+    import requests
+    redirect_uri = "http://localhost"
+
+    code = "Base64 code obtained from get_auth_from_user()"
+    token_url = "https://accounts.spotify.com/api/token"
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    data = {"grant_type": "authorization_code", "code": code, "redirect_uri": redirect_uri}
+    response = requests.post(token_url, headers=headers, data=f"grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&scope=user-read-playback-state user-modify-playback-state&code={code}&redirect_uri={redirect_uri}")
+
+    with open("spotify_tokens.json", 'wb') as fd:
+        for chunk in response.iter_content(chunk_size=128):
+            fd.write(chunk)
 
 def refresh_token():
     token_url = "https://accounts.spotify.com/api/token"
